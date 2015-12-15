d3ff767a1dac368576ec45ac4c435a1ead672298
younesboulkaddid <boulkaddid.younes@gmail.com>
2015-12-15 12:22:16 +0100

    [FEATURE] Add update methods

--- src/Orm/QueryManager.php
+++ src/Orm/QueryManager.php
@@ -28,20 +28,39 @@ class QueryManager implements  QueryInterface{
 		return $query;
 
 	}
-	public function update(){
+	public function update($methods){
 
+		$query = "UPDATE ".$this->getTable()." SET";
+
+		$numItems = count($methods);
+		$i = 0;
+
+		foreach($methods as $name => $method){
+
+			if(++$i === $numItems){
+				$query .= " `$name`=\"".$this->$method()."\" ";
+			}else{
+				$query .= " `$name`=\"".$this->$method()."\", ";
+			}
+		}
+
+		$query .= "WHERE `id`=".$this->getId()."; ";
+
+		return $query;
 	}
 	public function delete(){
-		$query = "DELETE FROM ".$this->getTable()." WHERE id=".$this->getId();
+		$query = "DELETE FROM ".$this->getTable()." WHERE id=".$this->getId().";";
 		$connexion = Orm::getConnexion();
 		$result = $connexion->query($query);
 	}
 	public function insert($methods){
 
 		$query = "INSERT INTO ".$this->getTable()." VALUE (''";
+
 		foreach($methods as $name => $method){
 			$query .= ", '".$this->$method()."'";
 		}
+
 		$query .= ")";
 		return $query;
 	}
@@ -54,7 +73,7 @@ class QueryManager implements  QueryInterface{
 
 		$table = $this->getTable();
 		$connexion = Orm::getConnexion();
-		$columns = $connexion->query("SELECT table_name, column_name, data_type FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = '$table' AND table_schema = 'orm'")->fetchAll();
+		$columns = $connexion->query("SELECT table_name, column_name, data_type FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = '$table' AND table_schema = 'orm';")->fetchAll();
 		$methods = [];
 
 		foreach( $columns as $e){
@@ -64,11 +83,17 @@ class QueryManager implements  QueryInterface{
 				$methods[$e['column_name']] = 'get'.ucfirst($e['column_name']);
 			};
 		}
+		if($this->getId() != null){
+			$query = $this->update($methods, $this);
+			$connexion->query($query);
+		}else{
+			$query = $this->insert($methods, $this);
+			$connexion->query($query);
+			$result = $connexion->lastInsertId();
+			$this->setId($result);
+		}
 
-		$query = $this->insert($methods, $this);
-		$connexion->query($query);
-		$result = $connexion->lastInsertId();
-		$this->setId($result);
+
 
 	}
 
